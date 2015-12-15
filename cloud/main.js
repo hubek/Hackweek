e1717d00f75082bf27b7dde45cae71897ba90d1b
Yamen Ali <yamen.ali@zalando-lounge.de>
2015-12-15 17:28:48 +0100

    initial commit

---
+++ cloud/main.js
@@ -0,0 +1,25 @@
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", function(request, response) {
+  response.success("Hello world!");
+});
+
+Parse.Cloud.define("test", function(request, response) {
+  
+  Parse.Cloud.useMasterKey();
+  var query = new Parse.Query( Parse.User);
+
+  //query.equalTo("name", request.params.name);
+
+  query.find({
+    success: function(results) {
+
+      response.success( results );
+    },
+    error: function() {
+      response.error("user lookup failed");
+    }
+  });
+
+});
