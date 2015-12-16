1f5b0bf48cc48299f41542dd90c07ee55629e6cc
Yamen Ali <yamen.ali@zalando-lounge.de>
2015-12-16 11:48:15 +0100

    coords api

--- cloud/ngo.js
+++ cloud/ngo.js
@@ -1,7 +1,7 @@
 //================== Create NGO ===================
 Parse.Cloud.define("create_ngo", function(request, response) {
 
-  var ngo = new Parse.NgoDetails();
+  var ngo = new Parse.Object( "NgoDetails" );
 
   ngo.set( "name", request.params.name );
   ngo.set( "description", request.params.description );
@@ -24,3 +24,32 @@ Parse.Cloud.define("create_ngo", function(request, response) {
 
 });
 //================== Create NGO ===================
+
+Parse.Cloud.define('myHttpRequest', function(request, response) {
+  
+  Parse.Cloud.httpRequest({
+  	
+  	method: "POST",
+  	url: 'https://maps.googleapis.com/maps/api/geocode/json',
+
+  	params: {
+  		address : "Bayreuther Straße 8, Berlin"
+    },
+    
+    success: function(httpResponse) {
+    	var res = httpResponse.data;
+    	if(res.status == "OK"){
+    		var langLat = res.results[0].geometry.location;
+            console.error(langLat);
+        }
+
+        response.success( httpResponse.data );
+    },
+
+    error: function(httpResponse) {
+    	console.error('Request failed with response code ' + httpResponse.status);
+    	response.error( httpResponse.status );
+    }
+  });
+
+});
