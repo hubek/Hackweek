de1b7a5da6a51eaf5b61d88ac6960d9846b5999c
Hubert Masiak <hubert.masiak@zalando-lounge.de>
2015-12-16 16:20:41 +0100

    ngos search by distance, count dist and order

--- cloud/search.js
+++ cloud/search.js
@@ -13,17 +13,39 @@ Parse.Cloud.define("ngoByDistance", function(request, response) {
       console.log("all NGOs by distance: " + results2)
       response.success(results2);
     }, function(error) {
-      response.error("category lookup failed");
+      response.error("all NGOs by distance failed");
     }
   );
 });
 
 function sortByDistance(lat, long, ngos) {
 	console.log("sortByDistance: ");
+	var newNgos = ngosWithDistance(lat, long, ngos);
+	newNgos.sort(compareByDistance);
+	return newNgos;				
+}
+
+function ngosWithDistance(lat, long, ngos) {
 	var newNgos = new Array();
 	for (var i = 0; i < ngos.length; i++) {
 		newNgos[i] = JSON.parse(JSON.stringify(ngos[i]));
-    newNgos[i].distance = 1;
+		var ngoLat = newNgos[i].coordinates.latitude;
+		var ngoLong = newNgos[i].coordinates.longitude
+    newNgos[i].distance = dist(long-ngoLong, lat-ngoLat);
   };		
-	return newNgos;				
+	return newNgos;	
+}
+
+function dist(x1,y1,x2,y2){ 
+  if(!x2) x2=0; 
+  if(!y2) y2=0;
+  return Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)); 
+}
+
+function compareByDistance(a,b) {
+  if (a.distance < b.distance)
+    return -1;
+  if (a.distance > b.distance)
+    return 1;
+  return 0;
 }
